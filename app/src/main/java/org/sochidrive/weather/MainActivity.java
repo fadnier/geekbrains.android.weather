b001022971c1eadbaaf5c57ad2fe7c0b56166529
fadnier <fadnier@sochidrive.org>
2020-07-25 12:16:29 +0300

    HomeWork1

--- app/src/main/java/org/sochidrive/weather/MainActivity.java
+++ app/src/main/java/org/sochidrive/weather/MainActivity.java
@@ -3,12 +3,24 @@ package org.sochidrive.weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageMainWeather;
+    TextView textMainDegree;
+    TextView textMainCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        imageMainWeather = findViewById(R.id.imageMainWeather);
+        textMainDegree = findViewById(R.id.textMainDegree);
+        textMainCity = findViewById(R.id.textMainCity);
     }
 }
